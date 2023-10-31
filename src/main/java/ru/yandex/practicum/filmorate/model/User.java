51ddf2940ad9646e0e5fb03eaff9782c2e08cdb9
Stepan4o <Stepan4oranec@gmail.com>
2023-10-31 20:46:19 +0200

    Добавил аннотацию @Email

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.exceptions.InvalidUserModelException;
 
+import javax.validation.constraints.Email;
 import java.time.LocalDate;
 
 @Data
@@ -11,6 +13,7 @@ public class User {
 
     private int id;
 
+    @Email(message = InvalidUserModelException.INCORRECT_EMAIL)
     private final String email;
 
     private final String login;
